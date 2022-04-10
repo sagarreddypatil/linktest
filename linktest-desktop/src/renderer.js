80d248cf723bd14025d894e543bf2af227b5a6a6
Sagar Patil <sagarreddypatil@gmail.com>
2022-04-10 04:02:11 -0400

    pretty ✨

--- linktest-desktop/src/renderer.js
+++ linktest-desktop/src/renderer.js
@@ -1,18 +1,24 @@
 const udp = require("dgram");
-const ip = require("ip");
 
 const discoveryPort = 42069;
 window.discovered = new Map();
 
 var deviceNameInput = document.getElementById("device-name");
+var searchButton = document.getElementById("search-button");
 function setDeviceName(name) {
   localStorage.setItem("deviceName", name);
 }
 function getDeviceName() {
-  return localStorage.getItem("deviceName") || "default";
+  let deviceName = localStorage.getItem("deviceName") || "";
+  deviceNameInput.value = deviceName;
+
+  return deviceName || "default";
 }
 
+getDeviceName();
+
 var server = udp.createSocket("udp4");
+server.bind(discoveryPort);
 server.on("error", (err) => {
   console.log(`Server error:\n${err.stack}`);
   server.close();
@@ -21,7 +27,6 @@ server.on("error", (err) => {
 function sendDiscovery(requestDiscovery, addrses) {
   let sendObj = {
     name: getDeviceName(),
-    ip: ip.address(),
     requestDiscovery: requestDiscovery,
   };
   console.log(`Sending discovery:\n${JSON.stringify(sendObj)}`);
@@ -39,15 +44,17 @@ function sendDiscovery(requestDiscovery, addrses) {
 
 deviceNameInput.onchange = (e) => {
   setDeviceName(e.target.value);
+};
+searchButton.onclick = (e) => {
   sendDiscovery(true);
 };
 
 server.on("message", (msg, rinfo) => {
   if (rinfo.port != discoveryPort) return;
 
-  let data = msg.toJSON();
+  let data = JSON.parse(msg.toString());
 
-  if (!("name" in data && "ip" in data && "requestDiscovery" in data)) {
+  if (!("name" in data && "requestDiscovery" in data)) {
     console.log("Invalid message:", data);
     return;
   }
@@ -57,9 +64,9 @@ server.on("message", (msg, rinfo) => {
     return;
   }
 
-  window.discovered.set(data.name, data.ip);
+  window.discovered.set(rinfo.address, data.name);
   generateDevicesTable();
-  console.log(`Discovered ${data.name} at ${data.ip}`);
+  console.log(`Discovered ${data.name} at ${rinfo.address}`);
 
   if (!data.requestDiscovery) return;
 
@@ -71,7 +78,7 @@ let devicesTable = document.getElementById("devices-table");
 function generateDevicesTable() {
   devicesTable.innerHTML = "";
 
-  for (let [name, ip] of window.discovered) {
+  for (let [ip, name] of window.discovered) {
     let row = document.createElement("tr");
     row.innerHTML = `<td>${name}</td><td>${ip}</td>`;
     devicesTable.appendChild(row);
