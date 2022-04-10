d0e2ed20a718c73c4327dd57fd48d1768170407d
Sagar Patil <sagarreddypatil@gmail.com>
2022-04-10 01:54:59 -0400

    ton of progress that im bout to delete

---
+++ renderer.js
@@ -0,0 +1,67 @@
+const udp = require("dgram");
+const ip = require("ip");
+
+const discoveryPort = 42069;
+window.discovered = new Map();
+
+var deviceNameInput = document.getElementById("device-name");
+function setDeviceName(name) {
+  localStorage.setItem("deviceName", name);
+}
+function getDeviceName() {
+  return localStorage.getItem("deviceName") || "default";
+}
+
+var server = udp.createSocket("udp4");
+server.on("error", (err) => {
+  console.log(`Server error:\n${err.stack}`);
+  server.close();
+});
+
+function sendDiscovery(requestDiscovery, addrses) {
+  let sendObj = {
+    name: getDeviceName(),
+    ip: ip.address(),
+    requestDiscovery: requestDiscovery,
+  };
+  console.log(`Sending discovery:\n${JSON.stringify(sendObj)}`);
+
+  let sendStr = JSON.stringify(sendObj);
+
+  if (requestDiscovery) {
+    server.setBroadcast(true);
+    server.send(sendStr, discoveryPort);
+  } else {
+    server.setBroadcast(false);
+    server.send(sendStr, discoveryPort, addrses);
+  }
+}
+
+deviceNameInput.onchange = (e) => {
+  setDeviceName(e.target.value);
+  sendDiscovery(true);
+};
+
+server.on("message", (msg, rinfo) => {
+  if (rinfo.port != discoveryPort) return;
+
+  let data = msg.toJSON();
+
+  if (!("name" in data && "ip" in data && "requestDiscovery" in data)) {
+    console.log("Invalid message:", data);
+    return;
+  }
+
+  if (data.name == "default") {
+    console.log("Invalid message:", data);
+    return;
+  }
+
+  window.discovered.set(data.name, data.ip);
+  console.log(`Discovered ${data.name} at ${data.ip}`);
+
+  if (!data.requestDiscovery) return;
+
+  console.log(`Sending discovery response to ${data.name}`);
+  sendDiscovery(false, rinfo.address);
+});
